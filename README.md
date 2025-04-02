2e95b9f1f098e00c27c719bd54f0114e247130ec
Laurita R <laurita.radisevska@mif.stud.vu.lt>
2025-04-02 14:40:39 +0300

    quick fix

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # dynamicElements
 
 The code in `test.py` waits about 12 sec to book the hotel at price $100 and solve the capture.
-The code in `test2.py` fills 2 different registration forms. One fails the test and the second passses it.
+The code in `test2.py` fills 2 different registration forms. The first one passes the test and the second fails it.
 ## Video
-[![Unit Test Video](image.png)](https://drive.google.com/file/d/1QGMEZr6vxTpISXPkfMkhzvrtF0Nx1tGD/view?usp=sharing)
+[![Unit Test Video](image.png)](https://drive.google.com/file/d/1rBdsPyIiSfUpm91CZ-Nxsro5FdrTZAQI/view?usp=sharing)
--- test2.py
+++ test2.py
@@ -13,21 +13,20 @@ class TestRegistration(unittest.TestCase):
     def setUpClass(cls):
        service=Service("chromedriver.exe")
        cls.driver = webdriver.Chrome(service=service)
-
     
     def fill_registration_form(self, registration_url):
         self.driver.get(registration_url)
         try:
             self.driver.find_element(By.XPATH, '/html/body/div/form/div[1]/div[1]/input').send_keys('Robotas')
             self.driver.find_element(By.XPATH, '/html/body/div/form/div[1]/div[2]/input').send_keys('Robotauskas')
-            # self.driver.find_element(By.XPATH, '/html/body/div/form/div[1]/div[3]/input').send_keys('robotas.robotauskas@mif.stud.vu.lt')
+            self.driver.find_element(By.XPATH, '/html/body/div/form/div[1]/div[3]/input').send_keys('robotas.robotauskas@mif.stud.vu.lt')
             self.driver.find_element(By.XPATH, '/html/body/div/form/div[2]/div[1]/input').send_keys('+37061234567')
             self.driver.find_element(By.XPATH, '/html/body/div/form/div[2]/div[2]/input').send_keys('Didlaukio g. 59, Vilnius')
             self.driver.find_element(By.XPATH, '/html/body/div/form/button').click()
-            self.driver.implicitly_wait(5)
         except NoSuchElementException:
-            self.fail("Test failed due to missing element")
-
+            return False
+        return True
+    
     def test_registration1(self):
         self.fill_registration_form("http://suninjuly.github.io/registration1.html")
         success_message= WebDriverWait(self.driver,5).until(
@@ -36,8 +35,6 @@ class TestRegistration(unittest.TestCase):
         self.assertEqual(success_message, 'Congratulations! You have successfully registered!')
         time.sleep(2)
 
-    
-    
     def test_registration2(self):
         self.fill_registration_form("http://suninjuly.github.io/registration2.html")
         success_message= WebDriverWait(self.driver,5).until(
